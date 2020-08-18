a44ce0d984c16ab4370b24a34a4f006f2190a35a
yiteng.liu <yiteng.liu@11bee.com>
2020-08-18 11:34:18 +0800

    update

--- src/main/java/icycream/common/registry/ScreenRegistryManager.java
+++ src/main/java/icycream/common/registry/ScreenRegistryManager.java
@@ -7,11 +7,13 @@ import icycream.common.gui.MixerContainer;
 import icycream.common.gui.RefrigeratorContainer;
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod.EventBusSubscriber(modid = IcyCream.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ScreenRegistryManager {
+    @SubscribeEvent
     public static void registerScreens(FMLClientSetupEvent event) {
         ScreenManager.registerFactory(MixerContainer.type, GuiMixerScreen::new);
         ScreenManager.registerFactory(RefrigeratorContainer.type, GuiRefrigeratorScreen::new);
